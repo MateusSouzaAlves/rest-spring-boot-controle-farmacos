d0295f41c5cabd4c952e144262c5ac05f75738b3
Mateus de Souza Alves <mateusgithub2@gmail.com>
2023-01-13 21:43:04 -0300

    Iniciando a aplicação com Spring web e DevTools

---
+++ RestSpringBootControleFarmacos/rest-spring-boot-controle-farmacos/src/main/java/br/com/farmacos/StartUp.java
@@ -0,0 +1,13 @@
+package br.com.farmacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartUp.class, args);
+	}
+
+}
---
+++ RestSpringBootControleFarmacos/rest-spring-boot-controle-farmacos/src/test/java/br/com/farmacos/StartUpTests.java
@@ -0,0 +1,13 @@
+package br.com.farmacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartUpTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
