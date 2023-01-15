7a2a4fe74fdbf270cbedba3a6370e4d931535c00
Mateus de Souza Alves <mateusgithub2@gmail.com>
2023-01-15 12:53:45 -0300

    Implementando verbo Post de remédios

---
+++ RestSpringBootControleFarmacos/rest-spring-boot-controle-farmacos/src/main/java/br/com/farmacos/remedio/DadosCadastroRemedio.java
@@ -0,0 +1,5 @@
+package br.com.farmacos.remedio;
+
+public record DadosCadastroRemedio(String nome, String via, String lote, String validade, Laboratorio laboratorio) {
+
+}
---
+++ RestSpringBootControleFarmacos/rest-spring-boot-controle-farmacos/src/main/java/br/com/farmacos/remedio/Laboratorio.java
@@ -0,0 +1,13 @@
+package br.com.farmacos.remedio;
+
+//Front end deve ser avisado sobre a necessidade de só aceitar caracteres maiúsculos
+
+public enum Laboratorio {
+	
+MEDLEY,
+EMS,
+ACHÉ,
+EUROFARMA,
+BAYER;
+
+}
