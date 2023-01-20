e919f69d49ddae3838b6880db40d50a330b3be86
Mateus de Souza Alves <mateusgithub2@gmail.com>
2023-01-20 18:41:08 -0300

    Implementando tratamento personalizado de erros

---
+++ RestSpringBootControleFarmacos/rest-spring-boot-controle-farmacos/src/main/java/br/com/farmacos/infra/TratadorDeErros.java
@@ -0,0 +1,33 @@
+package br.com.farmacos.infra;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<?> tratarErro404() {
+		return ResponseEntity.notFound().build();
+	}
+	
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<?> tratarErro400(MethodArgumentNotValidException ex) {
+		var erros = ex.getFieldErrors();
+		
+		return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+	}
+	
+	private record DadosErroValidacao(String campo, String mensagem) {
+		public DadosErroValidacao(FieldError erro) {
+			this(erro.getField(),erro.getDefaultMessage());
+		}
+	}
+}
+
