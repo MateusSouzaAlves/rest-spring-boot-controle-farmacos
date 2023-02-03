a56690fc2486ce466745d43a792c0a2386cd33c2
Mateus de Souza Alves <mateusgithub2@gmail.com>
2023-02-02 23:07:06 -0300

    Liberando o acesso ao swagger sem autorização via token

--- RestSpringBootControleFarmacos/rest-spring-boot-controle-farmacos/src/main/java/br/com/farmacos/infra/security/SecurityConfigurations.java
+++ RestSpringBootControleFarmacos/rest-spring-boot-controle-farmacos/src/main/java/br/com/farmacos/infra/security/SecurityConfigurations.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -40,4 +41,10 @@ public class SecurityConfigurations {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
+	 @Bean
+	 WebSecurityCustomizer webSecurityCustomizer() {
+	     return (web) -> web.ignoring().requestMatchers("/swagger-ui/**", "/v3/api-docs/**");
+	 }
+	
 }
